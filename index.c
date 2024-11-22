c078c17c0ca3c682e0654657ad5f7d8f1f7ac8de
ENAA <blkwiam2003@gmail.com>
2024-11-22 16:19:46 +0100

    start project

---
+++ index.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<stdlib.h>
+typedef struct{
+    char day[50];
+    char mounth[50];
+    char year[50];
+}date;
+typedef struct{
+    char titre[50];
+    char descreption[50];
+    char priorite[50];
+}tache;
+void ajouter(){}
+void afficher(){}
+void modifier(){}
+void supprimer(){}
+void filtrer(){}
+int main(){
+   return 0;}
+   
